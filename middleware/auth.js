f96bbaea20669b8944b42143d3ebc42706cdd960
Shivam Sanju <shvmsanju@gmail.com>
2022-06-04 10:41:28 +0530

    need to change auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,16 @@ const catchAsyncErrors = require('./catchAsyncErrors');
 const jwt = require('jsonwebtoken');
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  console.log(req.headers.cookie);
-  if (!req.cookies){
-    res.status(401).send({"success": false, "message":"Please login first"});
-  }else{
-    const token = req.cookies.token;
-    if (!token) {
-      res.status(401).send({"success": false, "message":"Please login first"});
-    }else{
-      const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-      next();
-    }
-  }
+  console.log(req.headers);
+  // if (!req.cookies){
+  //   res.status(401).send({"success": false, "message":"Please login first"});
+  // }else{
+  //   const token = req.cookies.token;
+  //   if (!token) {
+  //     res.status(401).send({"success": false, "message":"Please login first"});
+  //   }else{
+  //     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  //   }
+  // }
+  next();
 });
