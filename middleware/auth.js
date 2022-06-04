ed4a5c339da52c4e9cf21ca1a44b8847dea860e3
Shivam Sanju <shvmsanju@gmail.com>
2022-06-04 14:59:57 +0530

    changed req. token to req.headers.token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const catchAsyncErrors = require('./catchAsyncErrors');
 const jwt = require('jsonwebtoken');
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  console.log(req.token);
-  const token = req.token;
+  console.log(req.headers.token);
+  const token = req.headers.token;
     if (!token) {
       res.status(401).send({"success": false, "message":"Please login first"});
     }else{
