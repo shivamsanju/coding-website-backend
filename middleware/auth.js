643c659090c8784800f73d722027e7df0122932d
Shivam Sanju <shvmsanju@gmail.com>
2022-06-04 15:24:47 +0530

    added catch jwt error

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,14 +5,14 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   console.log(req.headers.token);
   const token = req.headers.token;
     if (!token) {
-      res.status(401).send({"success": false, "message":"Please login first"});
+      res.status(401).send({"success": false, "message":"Unauthorized"});
     }else{
       try{
         const decodedData = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedData.userId;
+        next();
       } catch(e){
-        req.user = -1; // not a valid user
+        res.status(401).send({"success": false, "message":"Unauthorized"});
       }
-      next();
     }
 });
